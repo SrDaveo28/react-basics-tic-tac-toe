a85c51a9dfc9ada3bcc8ba7db304128b1b5af840
SrDevO <76437512+SrDaveo28@users.noreply.github.com>
2023-12-04 09:00:42 -0300

    set winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,71 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
 const TURNS = {
   X: 'x',
   O: 'o',
 }
 
-const Square = ({children, updateBoard, index}) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+
   return (
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const checkWinner = (boardToCheck) => {
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo;
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a];
+    }
+  }
+}
+
 const App = () => {
-  const board = Array(9).fill(null);
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null); // null if no one wins, and false when is a tie
+
+  const updateBoard = (index) => {
+    // validate to not update the board when is already marked or the game is over
+    if (board[index] || winner) return;
+    // update the board
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    // check who wins
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
+  }
+
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
@@ -23,12 +76,22 @@ const App = () => {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
-                {index}
+                {board[index]}
               </Square>
             )
           })
         }
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
